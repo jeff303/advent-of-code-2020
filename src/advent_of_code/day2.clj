1eadc263d862352e13771c79eaafcca58a0253ee
Jeff Evans <jeffrey.wayne.evans@gmail.com>
2020-12-03 14:16:08 -0600

    Adding day 2 solution and tests

---
+++ src/advent_of_code/day2.clj
@@ -0,0 +1,45 @@
+(ns advent-of-code.day2
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
+
+(defn password-valid-part1
+  ;; in part1, the n-low and n-high represent a min/max number of occurrences per character, respectively
+  [requirements password]
+  (let [grouped-by-char (group-by identity (seq (char-array password)))]
+    (every?
+      true?
+      (for [[pwchar [min-occ max-occ]] requirements]
+        (<= min-occ (count (get grouped-by-char pwchar)) max-occ)))))
+
+(defn password-valid-part2
+  ;; in part2, the n-low and n-high represent two positions (1-based) where the character must occur (exactly once)
+  [requirements password]
+  (every?
+    true?
+    (for [[pwchar positions] requirements]
+      (=
+        1
+        (->> positions (map #(nth password (dec %))) (filter #(= pwchar %)) count)))))
+
+(defn get-requirements-and-passwords []
+  (let [input-text (slurp (io/resource "input_day2"))
+        input-lines (clojure.string/split-lines input-text)
+        parsed-lines (map #(re-matches #"^(\d+)-(\d+) (.): (.*)$" %) input-lines)]
+    (for [parsed-line parsed-lines]
+      (let [[_ n-low n-high pw-char pw] parsed-line
+            pw-requirement {(nth pw-char 0) [(Integer/parseInt n-low) (Integer/parseInt n-high)]}]
+        [pw-requirement pw]))))
+
+(defn day2-part1 []
+  (count
+    (filter
+      true?
+      (for [[req pw] (get-requirements-and-passwords)]
+        (password-valid-part1 req pw)))))
+
+(defn day2-part2 []
+  (count
+    (filter
+      true?
+      (for [[req pw] (get-requirements-and-passwords)]
+        (password-valid-part2 req pw)))))
--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -1,8 +1,14 @@
 (ns advent-of-code.core-test
   (:require [clojure.test :refer :all]
-            [advent-of-code.day1 :as day1]))
+            [advent-of-code.day1 :as day1]
+            [advent-of-code.day2 :as day2]))
 
 (deftest day1-test
-  (testing "day 1 solution"
+  (testing "day 1 solutions"
     (is (= 898299 (day1/day1-part1)))
     (is (= 143933922 (day1/day1-part2)))))
+
+(deftest day2-test
+  (testing "day 2 solutions"
+    (is (= 591 (day2/day2-part1)))
+    (is (= 335 (day2/day2-part2)))))
