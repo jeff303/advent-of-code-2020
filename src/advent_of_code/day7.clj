383b18d36fb7b2ed07a9ebc29115f08f62b0b8e7
Jeff Evans <jeffrey.wayne.evans@gmail.com>
2020-12-08 21:17:14 -0600

    Implementing day 7 functions, solutions, and tests

---
+++ src/advent_of_code/day7.clj
@@ -0,0 +1,60 @@
+(ns advent-of-code.day7
+  (:gen-class)
+  (:require [advent-of-code.util :as util]
+            [clojure.string :as str]
+            [clojure.core.reducers :as r]
+            [clojure.set :as s]))
+
+(defn input-line-to-rule [input-line]
+  (let [[_ start-color contain-rules] (re-matches #"(.*) bags contain (.*)" input-line)
+        contain-rule-seq (re-seq #"\d+[^,.]*(?= bag)" contain-rules)]
+    {start-color
+      (into
+        {}
+        (for [contain-rule contain-rule-seq]
+          (let [[num color] (str/split contain-rule #" " 2)]
+            [color (Integer. num)])))}))
+
+(defn parse-inputs-as-rule-map
+  ([]
+   (parse-inputs-as-rule-map "input_day7"))
+  ([res-file]
+   (let [input-lines (util/read-problem-input-as-lines res-file)
+         individual-rules (r/map input-line-to-rule input-lines)]
+     (reduce merge individual-rules))))
+
+(defn target-color-reachable [rules visited-colors start-color target-color]
+  (if
+    (= (count visited-colors) (count rules))
+    false
+    (let [next-visited (conj visited-colors start-color)
+          next-colors (filter #(not (contains? visited-colors %)) (keys (get rules start-color)))]
+      (some
+        true?
+        (for [next-color next-colors]
+          (or
+             (= target-color next-color)
+             (target-color-reachable rules next-visited next-color target-color)))))))
+
+(defn count-reachable-colors [rules target-color]
+  (reduce
+    +
+    (pmap
+      #(if (target-color-reachable rules #{} % target-color) 1 0)
+      (keys rules))))
+
+(defn day7-part1 []
+  (count-reachable-colors (parse-inputs-as-rule-map) "shiny gold"))
+
+(defn count-total-reachable-bags [rules visited-colors start-color]
+  (let [next-visited (conj visited-colors start-color)
+        next-colors (get rules start-color)
+        minus-visited (apply dissoc next-colors visited-colors)
+        sum-nested-fn (fn [rule]
+                        (let [[color num] rule
+                              res (count-total-reachable-bags rules next-visited color)]
+                          (+ num (* num res))))]
+      (reduce + (pmap sum-nested-fn minus-visited))))
+
+(defn day7-part2 []
+  (count-total-reachable-bags (parse-inputs-as-rule-map) #{} "shiny gold"))
--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -6,7 +6,8 @@
             [advent-of-code.day3 :as day3]
             [advent-of-code.day4 :as day4]
             [advent-of-code.day5 :as day5]
-            [advent-of-code.day6 :as day6]))
+            [advent-of-code.day6 :as day6]
+            [advent-of-code.day7 :as day7]))
 
 (deftest day1-test
   (testing "day 1 solutions"
@@ -46,3 +47,29 @@
   (testing "day 6 solutions"
     (is (= 6778 (day6/day6-part1)))
     (is (= 3406 (day6/day6-part2)))))
+
+(deftest day7-test
+  (testing "day 7 functions"
+    (let [rule (day7/input-line-to-rule "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.")]
+      (is (= {"vibrant plum" {"faded blue" 5 "dotted black" 6}} rule)))
+    (let [rules (day7/parse-inputs-as-rule-map "test_day7_input")
+          vc #{}
+          target-color "shiny gold"]
+      (is (= {"bright white" {"shiny gold" 1}
+              "dark olive" {"dotted black" 4 "faded blue" 3}
+              "dark orange" {"bright white" 3 "muted yellow" 4}
+              "dotted black" {}
+              "faded blue" {}
+              "light red" {"bright white" 1 "muted yellow" 2}
+              "muted yellow" {"faded blue" 9 "shiny gold" 2}
+              "shiny gold" {"dark olive" 1 "vibrant plum" 2}
+              "vibrant plum" {"dotted black" 6 "faded blue" 5}} rules))
+      (is (= true (day7/target-color-reachable rules vc "light red" target-color)))
+      (is (= true (day7/target-color-reachable rules vc "bright white" target-color)))
+      (is (nil? (day7/target-color-reachable rules vc "vibrant plum" target-color))))
+    (let [rules-pt2 (day7/parse-inputs-as-rule-map "test_day7_part2_input")
+          vc #{}]
+      (is (= 126 (day7/count-total-reachable-bags rules-pt2 vc "shiny gold")))))
+  (testing "day 7 solutions"
+    (is (= 172 (day7/day7-part1)))
+    (is (= 39645 (day7/day7-part2)))))
